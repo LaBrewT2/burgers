386c2c02cd468448093dd0a340b7764d3346e9be
LaBrew T2 <labrewt2@hawkmail.newpaltz.edu>
2021-01-24 14:08:34 -0500

    Moved files from previous repo

---
+++ .history/db/seeds_20210124132141.sql
@@ -0,0 +1,4 @@
+USE burger
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
---
+++ .history/server_20210124133908.js
@@ -0,0 +1,28 @@
+
+//Express
+var express = require("express");
+
+var PORT = process.env.PORT || 8000;
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+///Handelbars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Listening on port: http://locs", PORT);
+});
